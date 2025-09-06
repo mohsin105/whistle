373da26f38afec044f72ad1c38a5824340d6d571
Mohsin <mohsinibnaftab@gmail.com>
2025-09-07 00:30:11 +0600

    adding id field in User Serializer

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer,
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         model=User
-        fields=['id','first_name','last_name','email','bio','location','phone_number','password']
+        fields=['first_name','last_name','email','bio','location','phone_number','password']
 
 class UserSerializer(BaseUserSerializer):
     profile_image=serializers.ImageField()
@@ -13,7 +13,7 @@ class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         ref_name = 'CustomUser'
         model=User
-        fields=['first_name','last_name','email','bio','location','phone_number','profile_image','cover_photo']
+        fields=['id','first_name','last_name','email','bio','location','phone_number','profile_image','cover_photo']
 
 class SimpleUserViewSerializer(BaseUserCreateSerializer):
     full_name=serializers.SerializerMethodField(method_name='user_full_name')
